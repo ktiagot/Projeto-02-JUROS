5b52452ca98cefcbdbeac7acb07ad46d3f397f1b
Nilton Mitsuharu Sugawara <32663400+niltonSugawara@users.noreply.github.com>
2018-03-24 23:09:46 -0300

    Update HomeServlet.java

--- Projeto02_juros/src/java/br/com/fatecpg/ads/poo/grupo06/juros/HomeServlet.java
+++ Projeto02_juros/src/java/br/com/fatecpg/ads/poo/grupo06/juros/HomeServlet.java
@@ -37,7 +37,7 @@ public class HomeServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet HomeServlet</title>");            
+            out.println("<title>Calculo de Juros Simples e Compostot</title>");            
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet HomeServlet at " + request.getContextPath() + "</h1>");
